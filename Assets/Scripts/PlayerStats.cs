68f495fe05b7790420b9009070384b02d8f02412
NSaneQTip <36085380+NSaneQTip@users.noreply.github.com>
2022-10-07 12:55:53 -0700

    Basic Player Stats.
    Added some basic player stats and linked the C# script to the PlayerMovement.cs script.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int health = 100;
+    public int currentHealth;
+    public Boolean isDead = false;
+    public float movementSpeed = 6.0f; // Adjust the speed as needed. May relegate this to a private variable.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
