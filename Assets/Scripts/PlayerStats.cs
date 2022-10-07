912ac35fe6a1a5a63abef953eb5d89cbeff7470b
NSaneQTip <36085380+NSaneQTip@users.noreply.github.com>
2022-10-07 13:02:13 -0700

    Update PlayerStats.cs

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    public int health = 100;
-    public Boolean isDead = false;
-    public float movementSpeed = 6.0f; // Adjust the speed as needed. May relegate this to a private variable.
+    public int health = 100; // Same logic here: since items will affect this, keep it public so everyone and everything can modify it to some degree.
+    public Boolean isDead = false; // For testing reasons, I am keeping this modifer public.
+    public float movementSpeed = 6.0f; // Adjust the speed as needed. Since this will be affected by items, let's keep it public.
     // Start is called before the first frame update
     void Start()
     {
