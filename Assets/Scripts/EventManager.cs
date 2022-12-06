c28881b199ffb52cf48989047c9df4443939d221
Alonzo Castanon <alonzocastanon@live.com>
2022-12-05 22:02:07 -0800

    Fix bug where next scene was triggering even when enemy touched exit object, connect it to existing VictoryConditions script

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class EventManager : MonoBehaviour {
     public delegate void EnemyDeath();
 
+    public delegate void VictoryObjectTouched();
+
     public static event EnemyDeath OnEnemyDeath;
+    public static event VictoryObjectTouched OnVictoryObjectTouched;
 
     public static void ReportEnemyDeath() {
         // Why use events instead of just making each enemy
@@ -13,5 +16,9 @@ public class EventManager : MonoBehaviour {
         // Because each enemy takes more memory to store that reference,
         // so its better to use events in this case
         OnEnemyDeath?.Invoke();
+    }
+
+    public static void ReportVictoryObjectTouched() {
+        OnVictoryObjectTouched?.Invoke();
     }
 }
